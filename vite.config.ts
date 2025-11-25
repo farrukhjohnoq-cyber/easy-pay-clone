438d0e7849edbad0a4208375eeb5f103e364a10b
farrukhjohnoq-cyber <farrukhjohnoq@gmail.com>
2025-11-25 23:47:56 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/easy-pay-clone/",  
   server: {
     host: "::",
     port: 8080,
